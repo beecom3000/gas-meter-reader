36785c73e814946a47edb4ae1ed67e9a6bda2fe4
beecom <bee.nakhakes@gmail.com>
2025-04-30 23:25:08 +1000

    use resource to load cascade classifier file

--- gmr-api/src/main/java/au/com/dobotics/gmr/config/OpenCvConfig.java
+++ gmr-api/src/main/java/au/com/dobotics/gmr/config/OpenCvConfig.java
@@ -7,6 +7,8 @@ import org.opencv.objdetect.CascadeClassifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.net.URL;
+
 @Slf4j
 @Configuration
 public class OpenCvConfig {
@@ -18,8 +20,12 @@ public class OpenCvConfig {
 
     @Bean
     public CascadeClassifier cascadeClassifier() {
+        URL resource = getClass().getResource("/models/haarcascade_frontalface_default.xml");
+        assert resource != null : "Failed to find cascade classifier file";
+        String filename = resource.getFile();
+        log.info("Loading cascade classifier file from {}", filename);
         CascadeClassifier cascadeClassifier = new CascadeClassifier();
-        cascadeClassifier.load("src/main/resources/models/haarcascade_frontalface_default.xml");
+        cascadeClassifier.load(filename);
         if (cascadeClassifier.empty()) {
             log.error("Failed to load cascade classifier");
             throw new RuntimeException("Failed to load cascade classifier");
